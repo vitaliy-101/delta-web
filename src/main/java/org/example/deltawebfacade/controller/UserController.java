9e63c2b8546aac15840a6858dd966cb804cd1f71
6ocra3 <6o3kmm@mail.ru>
2024-08-09 00:00:12 +0300

    add User Controller with getUser and fix CORS problem

---
+++ src/main/java/org/example/deltawebfacade/controller/UserController.java
@@ -0,0 +1,35 @@
+package org.example.deltawebfacade.controller;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.AllArgsConstructor;
+import org.example.deltawebfacade.dto.UserResponse;
+import org.example.deltawebfacade.service.auth.JwtService;
+import org.example.deltawebfacade.service.auth.UserDetailsServiceImpl;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+@Tag(name = "Информация о пользователе")
+public class UserController {
+
+    private final JwtService jwtService;
+    private final UserDetailsServiceImpl UserDetailsServiceImpl;
+    private final UserDetailsServiceImpl userDetailsServiceImpl;
+
+    @GetMapping("/user")
+    @Operation(summary = "Получить информацию о пользователе по токену")
+    public ResponseEntity<UserResponse> getUserInfo(@RequestHeader("Authorization") String token){
+        String jwt = token.substring(7);
+        String email = jwtService.extractUsername(jwt);
+        UserDetails userDetails = userDetailsServiceImpl.loadUserByUsername(email);
+        UserResponse userResponse = new UserResponse(userDetails.getUsername());
+        return ResponseEntity.ok(userResponse);
+    }
+
+
+}
